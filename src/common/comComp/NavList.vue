7d461d981fe121ef9bcdb3f8a1df7d7b0ca6f47e
ZHUOYUNPENG <571979865@qq.com>
2019-10-28 15:44:44 +0800

    修改了navlistaa

--- src/common/comComp/NavList.vue
+++ src/common/comComp/NavList.vue
@@ -4,7 +4,7 @@
       <i class="iconfont icon-tianchongxing-"></i>
       首页
     </router-link>
-    <router-link tag="li" to="/classifySortss">
+    <router-link tag="li" to="/classifySortssss">
       <i class="iconfont icon-fenlei"></i>
       分类
     </router-link>
