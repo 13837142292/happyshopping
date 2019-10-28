6ea59fc737dc357e539a3bdf8d5efd78f859cfd3
tuantuan <111@qq.com>
2019-10-28 16:39:23 +0800

    add config.js

--- src/components/ShoppingCart/shoppingCart.vue
+++ src/components/ShoppingCart/shoppingCart.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        shoppingCart
+        <div class="left">
+             <p>购物车(4)</p>
+        </div>
+      
     </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.left{
+    width: 100%;
+    height: 40px;
+    text-align: center;
+    font-size: 16px;
+}
+</style>
